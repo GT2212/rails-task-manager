0b7913354955865186b6696134b56eeae3a5172c
gabi <gabrielteodorescu2212@yahoo.com>
2024-03-06 09:45:16 +0100

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +12,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2020_06_09_171803) do
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.boolean "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_200_609_171_803) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.text 'details'
+    t.boolean 'completed'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
